aeab2d19f7fa4ade6a6689773a5ab3496204102d
BradJCoutts <81317063+BradJCoutts@users.noreply.github.com>
2021-07-22 22:31:37 +0200

    Add files via upload

--- recommenders/collaborative_based.py
+++ recommenders/collaborative_based.py
@@ -46,20 +46,22 @@ from sklearn.metrics.pairwise import cosine_similarity
 movies = pd.read_csv("resources/data/movies.csv")
 ratings = pd.read_csv("resources/data/ratings.csv")
 
-final_dataset = ratings.pivot(index='movieId',columns='userId',values='rating')
-final_dataset.fillna(0,inplace=True)
-no_user_voted = ratings.groupby('movieId')['rating'].agg('count')
-no_movies_voted = ratings.groupby('userId')['rating'].agg('count')
-final_dataset=final_dataset.loc[:,no_movies_voted[no_movies_voted > 50].index]
-final_dataset = final_dataset.loc[no_user_voted[no_user_voted > 10].index,:]
-csr_data = csr_matrix(final_dataset.values)
-final_dataset.reset_index(inplace=True)
-
-knn = NearestNeighbors(metric='cosine', algorithm='brute', n_neighbors=20, n_jobs=-1)
-knn.fit(csr_data)
 
 
-def get_movie_recommendation(movie_name):
+def get_movie_recommendation(movie_name,movies=movies):
+    final_dataset = ratings.pivot(index='movieId',columns='userId',values='rating')
+    final_dataset.fillna(0,inplace=True)
+    no_user_voted = ratings.groupby('movieId')['rating'].agg('count')
+    no_movies_voted = ratings.groupby('userId')['rating'].agg('count')
+    final_dataset=final_dataset.loc[:,no_movies_voted[no_movies_voted > 50].index]
+    final_dataset = final_dataset.loc[no_user_voted[no_user_voted > 10].index,:]
+    csr_data = csr_matrix(final_dataset.values)
+    final_dataset.reset_index(inplace=True)
+
+    knn = NearestNeighbors(metric='cosine', algorithm='brute', n_neighbors=20, n_jobs=-1)
+    knn.fit(csr_data)
+
+    
     n_movies_to_reccomend = 10
     movie_list = movies[movies['title']==movie_name]  
     if len(movie_list)>0:        
