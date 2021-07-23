7e534c7f05a52b52b094a1fcb2f808229a003c1d
BradJCoutts <81317063+BradJCoutts@users.noreply.github.com>
2021-07-23 15:09:21 +0200

    Add files via upload

--- recommenders/collaborative_based.py
+++ recommenders/collaborative_based.py
@@ -45,10 +45,6 @@ ratings = pd.read_csv("resources/data/ratings.csv")
 
 final_dataset = ratings.pivot(index='movieId',columns='userId',values='rating')
 final_dataset.fillna(0,inplace=True)
-no_user_voted = ratings.groupby('movieId')['rating'].agg('count')
-no_movies_voted = ratings.groupby('userId')['rating'].agg('count')
-final_dataset=final_dataset.loc[:,no_movies_voted[no_movies_voted > 50].index]
-final_dataset = final_dataset.loc[no_user_voted[no_user_voted > 10].index,:]
 csr_data = csr_matrix(final_dataset.values)
 final_dataset.reset_index(inplace=True)
 
